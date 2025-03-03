d595703ed6b705d5bc918f958edd0191ce9377eb
= <=>
2025-03-03 10:30:36 +0700

    feat: jb-4, Praktikum 2.4, Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,16 +5,6 @@
     </head> 
     <body> 
         <h1>Data User</h1> 
-
-        <table border="1" cellpadding="5" cellspacing="0"> 
-            <tr> 
-                <th>Jumlah Pengguna</th> 
-            </tr> 
-            <tr> 
-                <td>{{ $userCount }}</td> 
-            </tr> 
-        </table> 
-{{-- 
         <table border="1" cellpadding="2" cellspacing="0"> 
             <tr> 
                 <th>ID</th> 
@@ -22,16 +12,12 @@
                 <th>Nama</th> 
                 <th>ID Level Pengguna</th> 
             </tr> 
-
-            @foreach ($data as $user)
             <tr> 
-                <td>{{ $user->user_id }}</td> 
-                <td>{{ $user->username }}</td> 
-                <td>{{ $user->nama }}</td> 
-                <td>{{ $user->level_id }}</td> 
+                <td>{{ $data->user_id }}</td> 
+                <td>{{ $data->username }}</td> 
+                <td>{{ $data->nama }}</td> 
+                <td>{{ $data->level_id }}</td> 
             </tr> 
-            @endforeach
-
-        </table>  --}}
+        </table>  
     </body> 
 </html>
