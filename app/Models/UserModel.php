00462c0e54692f0547136f37e8ec7b3bd706a3cb
= <=>
2025-03-04 15:54:49 +0700

    feat: jb-4, Praktikum 2.7, Relationships

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class UserModel extends Model
 {
     use HasFactory;
@@ -12,10 +12,10 @@ class UserModel extends Model
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini 
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
 
-    protected $fillable = [
-        'username',
-        'nama',
-        'password',
-        'level_id'
-    ];
+    protected $fillable = ['username','nama','password','level_id'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
