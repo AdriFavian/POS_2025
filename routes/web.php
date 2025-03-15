2ec19456ff67b51d47f25acccb901db145764550
= <=>
2025-03-15 20:50:49 +0700

    feat: jb-06, Praktikum 3, Modal Ajax Hapus Data User

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::put('/{id}', [UserController::class, 'update']); // menyimpan perubahan data user 
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // Menampilkan halaman form edit user Ajax 
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // Menyimpan perubahan data user Ajax
+    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // untuk menampilkan konfirmasi hapus user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // untuk menghapus data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user 
 });
 
